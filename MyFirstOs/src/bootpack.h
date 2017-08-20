74aa4bbd98de1f5574c264799533beaa295ee25f
AcodeFamer <857033157@qq.com>
2017-08-20 16:17:27 +0800

    分割源文件，整理代码
    graph.c 关于画面的处理
    destbl.c 关于GDT、IDT的处理
    bookpack.c 其他处理

---
+++ MyFirstOs/src/bootpack.h
@@ -0,0 +1,116 @@
+
+struct BOOTINFO
+{
+	char cyls, leds, vmode, reserve;
+	short scrnx, scrny;
+	char *vram;
+};
+
+/*naskfunc.nas*/
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void write_mem8(int addr, int data);
+//执行LGDT命令
+void load_gdtr(int limit, int addr);
+//执行LLDT命令
+void load_idtr(int limit, int addr);
+
+/*graph.c*/
+
+//初始化调色板
+void init_palette(void);
+//设定调色板颜色
+void set_palette(int start, int end, unsigned char *rgb);
+//矩形颜色填充
+void boxfill8(unsigned char *vram, int xsize, unsigned char color, int x0, int y0, int x1, int y1);
+//初始化屏幕显示
+void init_screen(char *vram, int xsize, int ysize);
+//显示一个字符，颜色为c，在字库中地址为hankaku+（ascii）*16
+void printfont(char *vram, int xsize, int x, int y, char c, char *font);
+//显示字符串
+void printstring(char *vram, int xsize, int x, int y, char c, char *str);
+//初始化鼠标指针
+void init_mouse_cursor8(char *mouse, char bc);
+//设置图形在背景中显示，图形由buf定义，bxsize为宽度
+void show_graph_in_background(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize);
+
+//定义调色板中的颜色代号
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
+
+/*destbl.c*/
+
+//段描述符  8字节
+/*
+|   7    |     6       |     5     |   4    |   3    |   2    |   1    |   0    |  字节
+|76543210|7 6 5 4 3210 |7 65 4 3210|76543210|76543210|76543210|76543210|76543210|  比特
+|--------|-|-|-|-|---- |-|--|-|----|--------|--------|--------|--------|--------|  占位
+|  BASE  |G|D|0|A|LIMIT|P|D |S|TYPE|<------- BASE 23-0 ------>|<-- LIMIT 15-0 ->|  含义
+|  31-24 | |/| |V|19-16| |P |
+           |B| |L|     | |L |
+
+BASE: 段基址，由上图中的两部分(BASE 31-24 和 BSE23-0)组成
+G：LIMIT的单位，该位 0 表示单位是字节，1表示单位是 4KB
+D/B: 该位为 0 表示这是一个 16 位的段，1 表示这是一个 32 位段
+AVL: 该位是用户位，可以被用户自由使用
+LIMIT: 段的界限，单位由 G 位决定。数值上（经过单位换算后的值）等于段的长度（字节）- 1。
+P: 段存在位，该位为 0 表示该段不存在，为 1 表示存在。
+DPL：段权限
+S: 该位为 1 表示这是一个数据段或者代码段。为 0 表示这是一个系统段（比如调用门，中断门等）
+TYPE: 根据 S 位的结果，再次对段类型进行细分。详见：http://www.cnblogs.com/yangang92/p/7396968.html
+*/
+struct SEGMENT_DESCRIPTOR
+{
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+
+//中断描述符表
+/*
+|   7    |     6  |     5     |   4    |   3    |   2    |   1    |   0    |  字节
+|76543210|76543210|7 65 4 3210|76543210|76543210|76543210|76543210|76543210|  比特
+|--------|--------|-|--|-|----|--------|--------|--------|--------|--------|  占位
+|<--offset31-16-->|P|D |S|TYPE|<---0-->|----selector --->|<-- offset15-0 ->|  含义
+|                   |P |
+                  | |L |
+
+offset:中断处理函数的地址32位
+selector：中断处理函数的所在的段选择子
+Type：门类型，这里是中断门  1110b
+S：类似gdt该位，中断门为0
+DPL：段权限
+P：设置为0代表未被使用的中断
+ */
+struct GATE_DESCRIPTOR
+{
+	short offset_low, selector;
+
+	char dw_count, access_right;
+	short offset_high;
+};
+
+//初始化gdt idt
+void init_gdtidt(void);
+//设定gdt
+void set_segment_desc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+//设定ldt
+void set_gate_desc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
---
+++ MyFirstOs/src/graph.c
@@ -0,0 +1,165 @@
+#include "bootpack.h"
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:黒 */
+		0xff, 0x00, 0x00,	/*  1:亮红 */
+		0x00, 0xff, 0x00,	/*  2:亮绿 */
+		0xff, 0xff, 0x00,	/*  3:亮黄 */
+		0x00, 0x00, 0xff,	/*  4:亮蓝 */
+		0xff, 0x00, 0xff,	/*  5:亮紫*/
+		0x00, 0xff, 0xff,	/*  6:浅亮蓝 */
+		0xff, 0xff, 0xff,	/*  7:白 */
+		0xc6, 0xc6, 0xc6,	/*  8:亮灰 */
+		0x84, 0x00, 0x00,	/*  9:暗红 */
+		0x00, 0x84, 0x00,	/* 10:暗緑 */
+		0x84, 0x84, 0x00,	/* 11:暗黄 */
+		0x00, 0x00, 0x84,	/* 12:暗青 */
+		0x84, 0x00, 0x84,	/* 13:暗紫 */
+		0x00, 0x84, 0x84,	/* 14:暗浅蓝 */
+		0x84, 0x84, 0x84	/* 15:暗灰 */
+	};
+	set_palette(0, 15, table_rgb);
+
+	return;
+}
+
+/*调色板的访问步骤
+1.屏蔽中断
+2.设定的调色板号码写入0x03c8
+3.按RGB依次写入0x03c9
+4.如果想继续设定下一个调色板，省略调色板号，直接写依次写RGB*/
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();  //获取中断许可标志的值
+	io_cli();         //禁止中断
+	io_out8(0x03c8, start);
+	for(i = start; i <= end ;i++)
+	{
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+}
+
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char color, int x0, int y0, int x1, int y1)
+{
+	int x, y;
+    for(y = y0; y <= y1; y++)
+    {
+    	for(x = x0;x <= x1; x++)
+    	{
+    		vram[y * xsize + x] = color;
+    	}
+    }
+    return ;
+}
+
+
+void init_screen(char *vram, int xsize, int ysize)
+{
+	boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
+
+	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+	return;
+}
+
+void printfont(char *vram, int xsize, int x, int y, char c, char *font)
+{
+	int i;
+	char *p, d /* data */;
+	for (i = 0; i < 16; i++) {
+		p = vram + (y + i) * xsize + x;
+		d = font[i];
+		if ((d & 0x80) != 0) { p[0] = c; }
+		if ((d & 0x40) != 0) { p[1] = c; }
+		if ((d & 0x20) != 0) { p[2] = c; }
+		if ((d & 0x10) != 0) { p[3] = c; }
+		if ((d & 0x08) != 0) { p[4] = c; }
+		if ((d & 0x04) != 0) { p[5] = c; }
+		if ((d & 0x02) != 0) { p[6] = c; }
+		if ((d & 0x01) != 0) { p[7] = c; }
+	}
+	return;
+}
+
+void printstring(char *vram, int xsize, int x, int y, char c, char *str)
+{
+	extern char hankaku[4096];//使用字库
+	if(str == 0)
+		return;
+	int i = 0;
+	for( ; str[i] != '\0'; i++)
+	{
+		printfont(vram, xsize, x, y, c, hankaku + str[i] * 16);
+		x += 8;
+	}
+	return;
+}
+
+void init_mouse_cursor8(char *mouse, char bc)
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;//背景色
+			}
+		}
+	}
+	return;
+}
+
+void show_graph_in_background(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize)
+{
+	int x, y;
+	for (y = 0; y < pysize; y++) {
+		for (x = 0; x < pxsize; x++) {
+			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+		}
+	}
+	return;
+}
